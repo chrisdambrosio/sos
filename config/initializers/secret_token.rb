d18648781d1309b41be883981d0912295898d943
Chris D'Ambrosio <chris_dambrosio@playstation.sony.com>
2013-05-20 18:17:51 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pagernova::Application.config.secret_key_base = 'e79040b7af4af035c9180d79f12321dc3cd6b604b8feb4c34129bcc873320397ed74b2eef55bb5b5bf7efdd6e599f468295aa49867101c0dd16bf0165e08bb5b'
