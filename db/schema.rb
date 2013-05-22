2016d84f166c03978327ca77db9fce411ab3dd65
Chris D'Ambrosio <chris_dambrosio@playstation.sony.com>
2013-05-21 19:58:20 -0700

    added alert model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130522010933) do
+ActiveRecord::Schema.define(version: 20130522024556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "alerts", force: true do |t|
+    t.string   "description"
+    t.text     "details"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "contact_methods", force: true do |t|
     t.string   "label"
