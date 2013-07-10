d66ca23148f1700f6016aff199303e9ce5275e14
Chris D'Ambrosio <cdambrosio@gmail.com>
2013-07-10 00:22:36 -0700

    upgrade sucker punch / refactor jobs
    converted from old worker syntax to the new job
    syntax for v.1.0.0beta

--- spec/support/sucker_punch.rb
+++
@@ -1,5 +0,0 @@
-require 'sucker_punch/testing'
-
-SuckerPunch.config do
-  queue name: :alert_queue, worker: AlertWorker, workers: 3
-end
---
+++ spec/support/sucker_punch.rb.!sync
@@ -0,0 +1,5 @@
+require 'sucker_punch/testing'
+
+SuckerPunch.config do
+  queue name: :alert_queue, worker: AlertWorker, workers: 3
+end
